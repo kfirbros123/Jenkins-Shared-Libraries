70c5e340bbffeca7415d08774cad7979f2dc2df1
Kfir Bros <bobytb143@gmail.com>
2026-05-13 21:05:49 +0300

    sonar no exit plz and thank you

--- vars/sonarqubeLibrary.groovy
+++ vars/sonarqubeLibrary.groovy
@@ -1,5 +1,5 @@
 def sonarqubeScan(String projectKey, String projectName) {
     echo "Running SonarQube scan for project: ${projectName} with key: ${projectKey}..."
-    sh "exit 0" // Or exit 1 to breake the pipeline
+    //sh "exit 0" // Or exit 1 to breake the pipeline
     // Mock SonarQube scan logic here
 }
