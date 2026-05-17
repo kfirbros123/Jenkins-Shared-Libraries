e091bfd346ed9a8d7b0c27ef66b87467d395c8f0
kfirbros123 <bobytb143@gmail.com>
2026-05-17 18:45:18 +0300

    Implement SonarQube scan and project creation functions
    Added functions for SonarQube scanning and project creation.

--- vars/sonarqubeLibrary.groovy
+++ vars/sonarqubeLibrary.groovy
@@ -3,3 +3,25 @@ def sonarqubeScan(String projectKey, String projectName) {
     //sh "exit 0" // Or exit 1 to breake the pipeline
     // Mock SonarQube scan logic here
 }
+def sonarCreateProject(String projectKey) {
+        withSonarQubeEnv('SonarQubeScanner') {
+            sh """
+                curl -s -u ${env.SONAR_TOKEN}: \
+               -X POST "${env.SONAR_HOST_URL}/api/projects/create" \
+               -d "project=${projectKey}&name=${projectKey}"
+            """
+        }
+}
+ 
+def sonarLocalScan() {
+    def scannerHome = tool 'SonarQubeScanner'
+    withSonarQubeEnv('SonarQubeScanner') {
+        sh """
+            ${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectKey=${env.JOB_NAME} \
+            -Dsonar.projectName=${env.JOB_NAME} \
+            -Dsonar.sources=. \
+            -Dsonar.sourceEncoding=UTF-8
+        """
+    }
+}
