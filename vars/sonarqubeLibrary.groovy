6028fab2170deda0457b02a48f36fd88c07eacd6
kfirbros123 <bobytb143@gmail.com>
2026-05-17 19:58:33 +0300

    Update authentication token variable in SonarQube script

--- vars/sonarqubeLibrary.groovy
+++ vars/sonarqubeLibrary.groovy
@@ -9,7 +9,7 @@ def sonarCreateProject(String projectKey) {
         withSonarQubeEnv('SonarQubeScanner') {
             echo "creating project..."
             sh """
-                curl -s -u ${env.SONAR_TOKEN}: \
+                curl -s -u ${env.SONAR_AUTH_TOKEN}: \
                -X POST "${env.SONAR_HOST_URL}/api/projects/create" \
                -d "project=${projectKey}&name=${projectKey}"
             """
